5fff9374b4683149708abc2fbd9463b7e7dce326
gwagaen <90993599+gwagaen@users.noreply.github.com>
2021-09-19 20:06:50 +0900

    Initial commit

---
+++ src/cal.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class cal {
+    public static void main(String[] args) {
+        int op1, op2;
+        char operator;
+        Scanner inputstr = new Scanner(System.in);
+        System.out.print("Input: ");
+        String actualString = inputstr.nextLine();
+        actualString = actualString.replaceAll("\\s+", "");
+        StringTokenizer st = new StringTokenizer((actualString), "+-*/", true);
+        op1 = Integer.parseInt(st.nextToken());
+        operator = st.nextToken().charAt(0);
+        op2 = Integer.parseInt(st.nextToken());
+        
+        float res = 0;
+        if (operator == '+') {res=op1+op2;}
+        else if (operator == '-') {res=op1-op2;}
+        else if (operator == '*') {res=op1*op2;}
+        else if (operator == '/') {res=op1/op2;}
+        System.out.println(res);
+    }
+}
+
