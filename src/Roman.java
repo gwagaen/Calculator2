8d8bf93b07c313191af511d54037c810a4359794
gwagaen <90993599+gwagaen@users.noreply.github.com>
2021-09-19 21:30:43 +0900

    good

---
+++ src/Roman.java
@@ -0,0 +1,12 @@
+public class Roman {
+    public String rom1;
+    public String romop2;
+    public String rom3;
+    public String together(){
+
+        return rom1 + " " + romop2 + " " + rom3;
+
+
+    }
+
+}
--- src/cal.java
+++ src/cal.java
@@ -4,14 +4,27 @@ public class cal {
     public static void main(String[] args) {
         int op1, op2;
         char operator;
+        String var1, varop2, var3;
         Scanner inputstr = new Scanner(System.in);
         System.out.print("Input: ");
         String actualString = inputstr.nextLine();
         actualString = actualString.replaceAll("\\s+", "");
         StringTokenizer st = new StringTokenizer((actualString), "+-*/", true);
-        op1 = Integer.parseInt(st.nextToken());
-        operator = st.nextToken().charAt(0);
-        op2 = Integer.parseInt(st.nextToken());
+        var1 = st.nextToken();
+        varop2 = st.nextToken();
+        var3 = st.nextToken();
+        if (var3.contains("8")) {
+            Roman roman = new Roman();
+            roman.rom1 = var1;
+            roman.romop2 = varop2;
+            roman.rom3 = var3;
+            System.out.println(roman.together());
+            System.exit(0);
+        }
+
+        op1 = Integer.parseInt(var1);
+        operator = varop2.charAt(0);
+        op2 = Integer.parseInt(var3);
         if (1 <= op1 && 10 >= op1 && 1 <= op2 && 10 >= op2) {
         float res = 0;
         if (operator == '+') {res=op1+op2;}
@@ -20,6 +33,8 @@ public class cal {
         else if (operator == '/') {res=op1/op2;}
         System.out.println(res);
         }
-        }
+    }
+
+
 }
 
