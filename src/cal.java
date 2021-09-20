92c2b9044e8e37c258dd3e3a5ce93202469832d0
gwagaen <90993599+gwagaen@users.noreply.github.com>
2021-09-20 10:41:53 +0900

    good

--- src/cal.java
+++ src/cal.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
 import java.util.StringTokenizer;
-
 class Roman {
-    String rom1;
-    String romop2;
-    String rom3;
-    char operand = romop2.charAt(0);
+    public String rom1;
+    public String romop2;
+    public String rom3;
+    char oper = romop2.charAt(0);
     RomanToNumber rom1c = new RomanToNumber();
     RomanToNumber rom3c = new RomanToNumber();
-    int sum = rom1c.romanToDecimal(rom1) + operand + rom3c.romanToDecimal(rom3);
+    int sum = rom1c.romanToDecimal(rom1) + oper + rom3c.romanToDecimal(rom3);
     NumberToRoman sumr = new NumberToRoman ();
     public String together(){
         return sumr.intToRoman(sum);
     }
 }
-
-
 public class cal {
     public static void main(String[] args) {
         int op1, op2;
